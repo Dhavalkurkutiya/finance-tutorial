03a798316ad1094749fb973d6d8093e5459c602c
Dhaval Kurkutiya <kurkutiyadhaval30@gmail.com>
2024-05-13 19:07:19 +0530

    02:Authentication

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
    <div className="p-4">
-    <Button>Click me</Button>
+   <UserButton afterSignOutUrl="/" />
    </div>
   );
 }
