d834302929285019055025a0c0d19d3ef070b991
Dhaval Kurkutiya <kurkutiyadhaval30@gmail.com>
2024-05-17 19:36:56 +0530

    04:Header component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-   <div className="p-4">
-   <UserButton afterSignOutUrl="/" />
-   </div>
+    <div className="p-4">
+     Dashboard page
+    </div>
   );
 }
