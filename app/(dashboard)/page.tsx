681cd27830785948adf7207bbb77fe24cfa01343
Dhaval Kurkutuiya <Kurkutiyadhaval30@gmail.com>
2024-06-06 22:52:53 +0530

    06:Account get api

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 export default function Home() {
+
   return (
     <div className="p-4">
      Dashboard page
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { QueryProvider } from "@/providers/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Financial, This is your Financial Overview Report",
   description: "This is your Financial Overview Report",
-}; 
+};
 
 export default function RootLayout({
   children,
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <QueryProvider>{children}</QueryProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
